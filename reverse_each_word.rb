8229c52c2b30125c9a62810ae6ad28a7f6965064
Edward Horsey <edwardhorsey@me.com>
2020-02-29 17:19:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
   string_split=string.split(" ")
   
-  string_split = string_split.collect {}
+  string_split = string_split.collect {|word|
+    
+  }
   
   string
 end
